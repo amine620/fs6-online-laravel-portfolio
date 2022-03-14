f9be9edc7cdafbda92531c7aa4bf007b2c1b5a7d
amine620 <mohafidi1998@gmail.com>
2022-03-14 20:34:56 +0100

    partial views

---
+++ resources/views/FrontOffice/welcome.blade.php
@@ -0,0 +1,36 @@
+
+@extends('../layouts.app')
+
+@section('content')
+    
+
+        <!-- Navigation-->
+        @include('../partials._navigation')
+       
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+           @include('../partials._about')
+
+            <hr class="m-0" />
+            <!-- Experience-->
+        
+           @include('../partials._experience')
+            
+            <hr class="m-0" />
+            <!-- Education-->
+            @include('../partials._education')
+           
+            <hr class="m-0" />
+            <!-- Skills-->
+           @include('../partials/_skills')
+            <hr class="m-0" />
+            <!-- Interests-->
+           @include('../partials/_interests')
+           
+            <hr class="m-0" />
+            <!-- Awards-->
+           @include('../partials/_awards')
+            
+        </div>
+ @endsection
